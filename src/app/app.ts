780f3b65aab9b9b6f274c7f946d3940190488de9
vagnersantosdasilva <vagnersantosdasilva@gmail.com>
2025-06-28 17:02:34 -0300

    feat: add header in app

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+//import { RouterOutlet } from '@angular/router';
+import { Header } from "./components/header/header";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [ Header],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
